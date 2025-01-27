8ce318d9d38c7ed37a2e29b53fc6f61a87ae9c17
myingineer <myingineer@gmail.com>
2025-01-27 01:04:05 +0100

    made changes to the html, updated the js code and added a few styles in the css

--- easybank-master/script.js
+++ easybank-master/script.js
@@ -1,16 +1,23 @@
 const openButton = document.querySelector(".open");
-const closeButton = document.querySelector(".close");
 const sideBar = document.querySelector(".side-bar");
 const container = document.querySelector(".nxt");
 const pic = document.querySelector(".img2");
 
-openButton.addEventListener("click", () => {
-  sideBar.style.display = "flex";
-  container.style.opacity = "3%";
-  pic.style.opacity = "30%";
-});
-closeButton.addEventListener("click", () => {
-  sideBar.style.display = "none";
-  container.style.opacity = "100%";
-  pic.style.opacity = "100%";
+openButton.addEventListener('click', () => {
+  if (sideBar.style.display === '') {
+    sideBar.style.display = 'block';
+    openButton.src = './images/icon-close.svg';
+    document.querySelector('.img1').style.opacity = '60%';
+    document.body.addEventListener('click', (e) => {
+      if (!openButton.contains(e.target)) {
+        sideBar.style.display = '';
+        document.querySelector('img1').style.opacity = '';
+        openButton.src = './images/icon-hamburger.svg';
+      };
+    });
+  } else {
+    openButton.src = './images/icon-hamburger.svg';
+    sideBar.style.display = '';
+    document.querySelector('.img1').style.opacity = '';
+  };
 });
